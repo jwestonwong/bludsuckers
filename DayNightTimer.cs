568066088bdd8ce5402fd0ced43b37366d4c0a56
jwestonwong <jwestonwong@gmail.com>
2017-02-14 12:27:04 -0500

    Add files via upload
    These are the two scripts I've made so far, for item generation and for the day/night cycle. Both will be editable as we develop the systems further, but should be robust enough to build upon.

---
+++ DayNightTimer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightTimer : MonoBehaviour {
+
+	bool Night = true;
+	float startTime = 0;
+	float elapsedTime = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		startTime = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		elapsedTime = Time.time - startTime;
+		//print (elapsedTime);
+		//print (Time.time);
+		//print (startTime);
+
+		//This if statment controls how long the day/night phase is. Currently it is given equal weighting,
+		//but it can be changed to be asymetrical. Changing the integer below changes how long the day/night
+		//is in seconds.
+		if (elapsedTime > 5) {
+			if (Night == true) {
+				Night = false;
+				print ("Day");
+				startTime = Time.time;
+			}
+			else if (Night == false) {
+				Night = true;
+				print ("Night");
+				startTime = Time.time;
+			}
+		}
+
+	}
+		
+}
---
+++ ItemGenScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is a list I put together to test it. Once we have the full item list and desired weightings, we just
+//edit it here, adding them to the list. We can use a state machine to control the effects of the items, I'll
+//hopefully have the time to add that tonight.
+public enum item {Stake, Garlic, Garlic_1, HolyWater, HolyWater_1, Trap, Trap2};
+
+public class ItemGenScript : MonoBehaviour {
+
+	item Item;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.I)) {
+			print ("Key pressed");
+			Item = (item)(UnityEngine.Random.Range(0, System.Enum.GetNames(typeof(item)).Length));
+			print (Item);
+		}
+
+	}
+		
+}
